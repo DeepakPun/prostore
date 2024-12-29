9e5b9641b0d805498d50fe9fa7e719568a695181
Deepak Pun <deepakpun@Deepaks-MacBook-Pro.local>
2024-12-29 18:11:29 -0500

    Product details page done. Initial commit for initial deployment

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+// const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+import ProductList from '@/components/shared/product/product-list'
+import { getLatestProducts } from '@/lib/actions/product.actions'
+
+const Homepage = async () => {
+	// await delay(5000)
+	const latestProducts = await getLatestProducts()
+	return (
+		<>
+			<ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
+		</>
+	)
+}
+
+export default Homepage
